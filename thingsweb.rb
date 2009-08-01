7dc55119cd7f17a9131aaff25003b17ffcbf23d5
dbr <dbr.onix@gmail.com>
2009-08-02 00:58:36 +0930

    Slight-start of async'ing the update of items

--- thingsweb.rb
+++ thingsweb.rb
@@ -1,9 +1,15 @@
 require 'rubygems'
 require 'sinatra'
+require 'json'
 
 require 'core/thingsdb'
 
 get('/') do
   @todo = ThingsDb::Todo.new
   erb :index
+end
+
+get('/by_focustype/:name') do
+  todos = ThingsDb::Todo.new.by_focustype(params['name'].to_sym)
+  "<pre>#{JSON.dump(todos.to_a)}</pre>"
 end
